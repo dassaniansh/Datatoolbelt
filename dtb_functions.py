d8aa7cc4d3672db952c8cf578d3381a5293fb810
veb7vmehra <veb7vmehra@gmail.com>
2022-09-29 21:44:03 +0530

    Test Data added

--- dtb_functions.py
+++ dtb_functions.py
@@ -6,12 +6,11 @@ import sys
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
+import xml.etree.ElementTree as ET
+from collections import OrderedDict
 
 #### Library Import Ends ####
-#### Global Variable Definition ####
-
-#### Global Variable Definition Ends ####
-#### Format Conversion ####
+#### Backend Stuff - NOT FOR ANSH ####
 
 def listTodic(lines, primeKey = None):
     output = {}
@@ -25,7 +24,7 @@ def listTodic(lines, primeKey = None):
             if lines[l][i][-1] == " " or lines[l][i][-1] == "\n":
                 lines[l][i] = lines[l][i][:-1]
     
-    if primeKey not None:
+    if primeKey != None:
         for i in range(len(lines[0])):
             if primeKey == lines[0][i]:
                 keyNum = i
@@ -36,11 +35,92 @@ def listTodic(lines, primeKey = None):
     output[primeKey] = {}
     for i in range(len(lines) - 1):
         output[primeKey][lines[i+1][keyNum]] = {}
-        for j in range(len(lines[0]) - 1):
-            if lines[0][j] not primeKey:
+        for j in range(len(lines[0])):
+            if lines[0][j] != primeKey:
                 output[primeKey][lines[i+1][keyNum]][lines[0][j]] = lines[i+1][j]
 
     return output
+
+def dicRec(dic, output, layer, prev = None):
+    if layer%2 == 0:
+        if output == []:
+            output.append(list(dic.keys()))
+        else:
+            for i in dic.keys():
+                if i not in output[0]:
+                    output[0].append(i)
+
+        for i in dic.keys():
+            if type(dic[i]) == OrderedDict:
+                dic[i] = dict(dic[i])
+
+            if type(dic[i]) == dict:
+                newlayer = layer + 1
+                output = dicRec(dic[i], output, newlayer, i)
+            else:
+                for k in range(len(output[0])):
+                    if output[0][k] == i:
+                        if len(output[0]) > len(output[-1]):
+                            for _ in range(len(output[0])-len(output[-1])):
+                                output[-1].append(None)
+                        
+                        if output[-1][k] == None:
+                            output[-1][k] = dic[i]
+                        else:
+                            t = [None] * len(output[0])
+                            output.append(t)
+                            output[-1][k] = dic[i]
+                        
+                        break
+    else:
+        for v in dic.keys():
+            for i in range(len(output[0])):
+                if output[0][i] == prev:
+                    if len(output[0]) > len(output[-1]):
+                        for _ in range(len(output[0])-len(output[-1])):
+                            output[-1].append(None)
+
+                    if output[-1][i] == None:
+                        output[-1][i] = v
+                    else:
+                        t = [None] * len(output[0])
+                        output.append(t)
+                        output[-1][i] = v
+                    
+                    break
+
+            if type(dic[v]) == OrderedDict:
+                dic[v] = dict(dic[v])
+                newlayer = layer+1
+                output = dicRec(dic[v], output, newlayer)
+
+    return output
+
+#### Backend Stuff - NOT FOR ANSH Ends ####
+
+#### Meta Data Creation ####
+
+def metaData(csvfile):
+    output = []
+    df = pd.read_csv(csvfile)
+    for col in df.columns:
+        temp = {}
+        temp["Title"] = col
+        temp["Type"] = df[col].dtypes.name
+        if df[col].dtypes.name == "int64" or df[col].dtypes.name == "float64":
+            temp["Average"] = df[col].mean()
+            temp["Max"] = df[col].max()
+            temp["Min"] = df[col].min()
+        else:
+            temp["Average"] = "NA"
+            temp["Max"] = "NA"
+            temp["Min"] = "NA"
+        output.append(temp)
+
+    return output
+
+
+#### Format Conversion ####
 
 def csvTojson(csvfile, jsonfile, primeKey = None):
     f = open(csvfile, "r", encoding="utf8")
@@ -53,50 +133,6 @@ def csvTojson(csvfile, jsonfile, primeKey = None):
         json.dump(output, outfile)
 
     return jsonfile
-
-
-def dicRec(dic, output, layer, prev = None):
-    if layer%2 == 0:
-        if output == []:
-            output.append(list(dic.keys()))
-        else:
-            for i in dic.keys():
-                if i not in output[0]:
-                    output[0].append(i)
-
-        for i in dic.keys():
-            if type(dic[i]) == dict:
-                layer += 1
-                output = dicRec(dic[i], output, layer, i)
-            else:
-                for k in len(range(output[0])):
-                    if output[0][k] == i:
-                        if output[-1][k] == None:
-                            output[-1][k] = dic[i]
-                        else:
-                            t = [None] * len(output[0])
-                            output.append(t)
-                            output[-1][k] = dic[i]
-                        
-                        break
-    else:
-        for v in dic.keys():
-            for i in len(range(output[0])):
-                if output[0][i] == prev:
-                    if output[-1][i] == None:
-                        output[-1][i] = v
-                    else:
-                        t = [None] * len(output[0])
-                        output.append(t)
-                        output[-1][i] = v
-                    
-                    break
-
-            if type(dic[v]) == dict:
-                layer += 1
-                output = dicRec(dic[v], output, layer)
-
-    return output
     
 
 def jsonTocsv(jsonfile, csvfile):
@@ -117,9 +153,12 @@ def jsonTocsv(jsonfile, csvfile):
 
 
 def xmlTocsv(xmlFile, csvfile):
-    contents = open(xmlFile).read()
-    data = xmltodict.parse("\"\"\""+contents+"\"\"\"")
-
+    parser = ET.XMLParser(encoding="utf-8")
+    tree = ET.parse(xmlFile, parser=parser)
+    xml_data = tree.getroot()
+    xmlstr = ET.tostring(xml_data, encoding='utf-8', method='xml')
+    data = dict(xmltodict.parse(xmlstr))
+    output = []
     layer = 0
     output = dicRec(data, output, layer)
 
@@ -130,17 +169,19 @@ def xmlTocsv(xmlFile, csvfile):
     return csvfile
 
 def xmlTojson(xmlFile, jsonfile):
-    contents = open(xmlFile).read()
-
-    output = xmltodict.parse("\"\"\""+contents+"\"\"\"")
-
+    parser = ET.XMLParser(encoding="utf-8")
+    tree = ET.parse(xmlFile, parser=parser)
+    xml_data = tree.getroot()
+    xmlstr = ET.tostring(xml_data, encoding='utf-8', method='xml')
+    output = dict(xmltodict.parse(xmlstr))
+    
     with open(jsonfile, "w", encoding="utf8") as outfile:
         json.dump(output, outfile)
 
     return jsonfile
 
 
-def csvToxml(csvfile, xmlFile):
+def csvToxml(csvfile, xmlFile, primeKey=None):
     f = open(csvfile, "r", encoding="utf8")
     lines = f.readlines()
     f.close()
@@ -266,11 +307,11 @@ def columnRep(csvfile, graph, pkey):
     df = pd.read_csv(csvfile)
     data = df[pkey]
     bins = 0
-    if type(data[1]) = int or type(data[1]) = float:
+    if type(data[1]) == int or type(data[1]) == float:
         mx = data.max()
         mn = data.min()
         bins = (mx - mn)
-    elif type(data[1]) = string:
+    elif type(data[1]) == str:
         bins = len(data.unique())
 
     fig = plt.hist(data, bins=bins)
@@ -295,3 +336,17 @@ def columnComp(csvfile, graph, colx, cols):
     return fig
 
 #### Data Visualization Ends ####
+
+
+#### Testing ####
+"""
+csvfile = "testdata.csv"
+jsonfile = "testdata.json"
+xmlfile = "testdata.xml"
+
+metaData(csvfile)
+#csvTojson(csvfile, jsonfile, "ID")
+#jsonToxml(jsonfile, xmlfile)
+#xmlTocsv(xmlfile, csvfile)
+"""
+#### Testing Ends ####
