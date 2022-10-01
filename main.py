ce86573c3e66c8c4f9e34a61340496b373c7f048
dassaniansh <anshdassani4@gmail.com>
2022-10-01 17:50:28 +0530

    basic routing

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sklearn.utils import column_or_1d
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 from datetime import datetime
-# from dtb_functions import *
+from dtb_functions import *
 
 app = Flask(__name__)
 
@@ -26,26 +26,35 @@ def upload_files():
       f = request.files['file']
       f.save(secure_filename(f.filename))
 
-    # prebuilt function
-    # conversion of file to csv()
+    # Conversion of files into CSV
+      if f.filename[-3:] == 'xml':
+       xmlTocsv(xmlFile= f.filename, csvfile=f'{f.filename[:-3]}csv')
+    
+      if f.filename[-4:] == 'json':
+       jsonTocsv(jsonfile=f.filename, csvfile=f'{f.filename[:-4]}csv')
 
     # task_id is created according to time
     # hour[2 digits] min[2 digits] sec[2 digits]
       task_id=str(datetime.now().time()).replace(':', '')[:6]
-      taskTofile[task_id]=f.filename
-      return f'file uploaded successfully with filename {taskTofile[task_id]} and task id is {task_id}'
+      if f.filename[-3:] == 'csv':
+        taskTofile[task_id]=f.filename
+        return f'file uploaded successfully with filename {taskTofile[task_id]} and task id is {task_id}'
+      else:
+        return 'Invalid file format, we only support csv, xml, json'
    except:
     return 'Something wrong occured'
 
 @app.route('/fetch/<task_id_for_file>')
 def fetch_file(task_id_for_file):
     # return meta data from database
-      return f'file uploaded successfully with filename with task id is {task_id_for_file}'
+    output = metaData(taskTofile[task_id_for_file])
+    return output
 
 @app.route('/fetch/<task_id_for_file>/<column>/<page>')
 def fetch_column(task_id_for_file, column, page):
     # getting column data from file
     data=taskTofile[task_id_for_file]
+    
     return f'file uploaded successfully with filename with task id is {task_id_for_file}'
 
 # main driver function
